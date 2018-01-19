926a6e9c7fe9220a4064abf950044295bbd0dfa7
Jarkko Rahikainen <JarkkoRahikainen@users.noreply.github.com>
2018-01-19 09:33:44 +0200

    Tasks 1-5 done

--- task01/integral_test.m
+++ task01/integral_test.m
@@ -18,3 +18,27 @@ for ii = [1e1, 1e2, 1e3, 1e4, 1e5]
     res = trapz(0:pi/ii:pi, sinvec);
     fprintf('Number of splits: %d, answer = %5.10f\n', ii, res)
 end
+
+disp('Testing integral_trapezoid_faster() with sin(x) in interval from 0 to pi (Ans = 2).')
+for ii = [1e1, 1e2, 1e3, 1e4, 1e5]
+    res = integral_trapezoid(@sin, 0, pi, ii);
+    fprintf('Number of splits: %d, answer = %5.10f\n', ii, res)
+end
+
+
+% Efficiency test:
+disp('Time for old version:')
+for ii=1:20
+    tic;
+    integral_trapezoid(@sin, 0, pi, 1e6);
+    tOld(ii) = toc;
+end
+fprintf('Total time %5.5f, average %5.5f\n', sum(tOld), mean(tOld))
+disp('After optimisation:')
+for ii=1:20
+    tic;
+    integral_trapezoid_faster(@sin, 0, pi, 1e6);
+    tNew(ii) = toc;
+end
+fprintf('Total time %5.5f, average %5.5f\n', sum(tNew), mean(tNew))
+fprintf('Total speedup: %5.2f\n', sum(tOld)/sum(tNew))
